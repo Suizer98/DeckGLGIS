89c41a0b3fd157019320e4bfc5c47fc1934d0f99
Suizer98 <93068036+Suizer98@users.noreply.github.com>
2024-12-15 16:20:26 +0800

    Renamed html title and so to DeckGL GIS

--- src/components/view/Topbar.tsx
+++ src/components/view/Topbar.tsx
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Topbar = () => {
   return (
     <Box as="header" className="w-full bg-gray-800 text-white p-4">
-      <h1 className="text-xl">CrystalBall GIS</h1>
+      <h1 className="text-xl">DeckGL GIS</h1>
     </Box>
   )
 }
